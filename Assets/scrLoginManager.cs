894eab3a163fbe1f3941de56ca99824e4e5860f8
Marquititititos <142502337+Marquititititos@users.noreply.github.com>
2024-05-27 15:48:11 -0300

    TP contra

--- Assets/scrLoginManager.cs
+++ Assets/scrLoginManager.cs
@@ -1,9 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class scrLoginManager : MonoBehaviour
 {
+
+    //cuando el usuario apriente el botón debe escribirse el mensaje en consola
+    //si el texto coincide con una contra predefinida escribir Access granted, sino Access denied
+
+    public InputField inputField;
+    public string password;
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +23,18 @@ public class scrLoginManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void Escribir()
+    {
+        Debug.Log(inputField.text);
+        
+        if (inputField.text == password)
+        {
+            Debug.Log("Acesso Granteado"); 
+        } else
+        {
+            Debug.Log("Acesso Deniedado");
+        }
     }
 }
